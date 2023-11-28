de16a0dfa9dc68667352b8cf9c04e0a23e2ee6d9
Ariel-Apple <arielapple98@gmail.com>
2023-11-28 17:50:45 -0300

    update

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { conn } = require('./src/db.js');
 const PORT = process.env.PORT || 3001;
 
 // Syncing all the models without force (preserve data).
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
   });
